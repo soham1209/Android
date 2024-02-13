3c7cc270fa770af211cb19047c7cbe9a0a6f368a
soham1209 <sohamns1234@gmail.com>
2024-02-13 21:50:50 +0530

    second commit

--- app/src/main/java/com/example/educationapp/MainActivity.java
+++ app/src/main/java/com/example/educationapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.educationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.educationapp.databinding.ActivityMainBinding;
 
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+                if(item.getItemId()== R.id.home) {
+                    replaceFragment(new HomeFragment());
+                }
+                else if(item.getItemId()== R.id.video) {
+                    replaceFragment(new VideoFragment());
+                }
+                else if(item.getItemId()== R.id.quizzes) {
+                    replaceFragment(new QuizzesFragment());
+                }
+                else if(item.getItemId()== R.id.setting) {
+                    replaceFragment(new SettingFragment());
+                }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutView,fragment);
+        fragmentTransaction.commit();
     }
 }
